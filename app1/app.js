46d802b675007da4157ab9e702015d036ca6d884
fabrizioca2 <111414325+fabrizioca2@users.noreply.github.com>
2025-06-05 01:16:34 -0500

    Update app.js

--- app1/app.js
+++ app1/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const Keycloak = require('keycloak-connect');
+
 const app = express();
 
 const memoryStore = new session.MemoryStore();
@@ -13,25 +14,31 @@ app.use(session({
 
 const keycloak = new Keycloak({ store: memoryStore });
 
-app.use(keycloak.middleware());
+app.use(keycloak.middleware({
+  logout: '/logout',
+  admin: '/'
+}));
 
 app.get('/', (req, res) => {
-  res.send(`<h2>App 1</h2>
+  res.send(`
+    <h2>App 1</h2>
     <a href="/private">Ir a área privada</a> |
-    <a href="http://localhost:3002">Ir a App 2</a>`);
+    <a href="http://localhost:3002">Ir a App 2</a>
+  `);
 });
 
 app.get('/private', keycloak.protect(), (req, res) => {
-  res.send(`<h2>Área privada App 1</h2>
+  res.send(`
+    <h2>Área privada App 1</h2>
     <pre>${JSON.stringify(req.kauth.grant.access_token.content, null, 2)}</pre>
-    <a href="/logout">Cerrar sesión</a>`);
+    <a href="/logout">Cerrar sesión</a>
+  `);
 });
 
 app.get('/logout', (req, res) => {
   req.session.destroy(() => {
-    res.redirect('http://localhost:8080/realms/DemoSSO/protocol/openid-connect/logout?redirect_uri=http://localhost:300w');
+    res.redirect('http://localhost:8080/realms/DemoSSO/protocol/openid-connect/logout?redirect_uri=http://localhost:3001');
   });
 });
 
-
-app.listen(3001, () => console.log('App 1 en http://localhost:3001'));
+app.listen(3001, () => console.log('App 1 corriendo en http://localhost:3001'));
